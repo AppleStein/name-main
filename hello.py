d7133116db3c22b706116b123324b0bcff6d66c2
AppleStein <pianonono1129@gmail.com>
2022-04-17 17:18:16 +0900

    スッキリさせた

--- hello.py
+++ hello.py
@@ -1,10 +1,7 @@
 def func():
-    print('     This is func() in hello.py')
     print('     __name__ is', __name__)
 
 
 #他からインポートされた場合は実行されない
 if __name__ == '__main__': #コマンドから実行→mainが入る
-    print("Start if __name__ == '__main__'")
-    print('call func()')
     func()
